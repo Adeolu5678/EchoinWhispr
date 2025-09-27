e0bd506264356c75bf5ea7a0f1ff19793a062a26
Emmanuel Adeoye <emmanueladeoye5678@gmail.com>
2025-09-28 00:40:02 +0100

    FIx - Vercel Deployment Error

--- Web/src/app/(main)/page.tsx
+++ Web/src/app/(main)/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Suspense } from 'react';
 import { WhisperList } from '@/features/whispers/components/WhisperList';
 import { useWhispers } from '@/features/whispers/hooks/useWhispers';
